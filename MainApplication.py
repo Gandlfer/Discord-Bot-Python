36db1331b14bf19ede7c4c487c074133c1a0f406
Darryl Lee <darryllee1999@gmail.com>
2021-05-31 00:11:46 +0800

    Find working dir

--- MainApplication.py
+++ MainApplication.py
@@ -2,14 +2,14 @@ import os
 
 import discord
 
-
+print(os.getcwd())
 token = open("bot-token","r").read()
 
-client = discord.Client()
+# client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+# @client.event
+# async def on_ready():
+#     print(f'{client.user} has connected to Discord!')
 
 
-client.run(token)
+# client.run(token)
