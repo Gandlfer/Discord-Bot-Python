aa68d587adab804811740fc473b36a27ed3f0804
Darryl Lee <darryllee1999@gmail.com>
2021-05-31 13:01:03 +0800

    Read token

--- MainApplication.py
+++ MainApplication.py
@@ -2,15 +2,14 @@ import os
 
 import discord
 
-print(os.getcwd())
-print(os.path.dirname(__file__))
-#token = open(os.path.dirname(os.getcwd())+"/bot-token","r").read()
+token = open("bot-token","r").read()
+print(token)
 
-# client = discord.Client()
+client = discord.Client()
 
-# @client.event
-# async def on_ready():
-#     print(f'{client.user} has connected to Discord!')
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
 
 
-# client.run(token)
+client.run(token)
